dbe2de59a227cec71d9098da099ed6f65edb5f52
Becky <8b2@protonmail.com>
2022-01-05 10:22:08 +0000

    It's not exactly what I wanted
    ... but it's close enough.
    
    The idea is what I want.
    
    Druid already provides an impelementation of `ListIter` for
    `(S, Arc<Vec<T>>)`. Rather than writing an implementation of `ListIter`
    for `AppData`, is it possible to `lens` into `AppData` such that `List`
    is looking at `(String, usize)`?

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
 #![windows_subsystem = "windows"]
 
+use std::ops::Deref;
 use std::sync::Arc;
 
 use druid::widget::prelude::*;
-use druid::widget::{Flex, Label, List, TextBox};
+use druid::widget::{Flex, Label, List, ListIter, TextBox};
 use druid::{AppLauncher, Data, Lens, WidgetExt, WindowDesc};
 
 const VERTICAL_WIDGET_SPACING: f64 = 20.0;
 
 #[derive(Clone, Data, Lens)]
 struct AppData {
-    internal: (String, Arc<Vec<usize>>),
+    filter: String,
+    list: Arc<Vec<usize>>,
+}
+
+impl ListIter<(String, usize)> for AppData {
+    fn for_each(&self, mut cb: impl FnMut(&(String, usize), usize)) {
+        for (i, item) in self.list.iter().enumerate() {
+            let d = (self.filter.clone(), item.to_owned());
+            cb(&d, i);
+        }
+    }
+
+    fn for_each_mut(&mut self, mut cb: impl FnMut(&mut (String, usize), usize)) {
+        let mut new_data: Option<Vec<usize>> = None;
+
+        for (i, item) in self.list.iter().enumerate() {
+            let mut d = (self.filter.clone(), item.to_owned());
+            cb(&mut d, i);
+
+            self.filter = d.0;
+
+            if !item.same(&d.1) {
+                match &mut new_data {
+                    Some(vec) => {
+                        vec[i] = d.1;
+                    }
+                    None => {
+                        let mut new = self.list.deref().clone();
+                        new[i] = d.1;
+                        new_data = Some(new);
+                    }
+                }
+            }
+        }
+        if let Some(vec) = new_data {
+            self.list = Arc::new(vec);
+        }
+    }
+
+    fn data_len(&self) -> usize {
+        self.list.len()
+    }
 }
 
 pub fn main() {
@@ -21,7 +63,8 @@ pub fn main() {
 
     // create the initial app state
     let initial_state: AppData = AppData {
-        internal: ("odd".into(), Arc::new(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+        filter: "odd".into(),
+        list: Arc::new(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
     };
 
     // start the application. Here we pass in the application state.
@@ -31,19 +74,27 @@ pub fn main() {
 }
 
 fn build_root_widget() -> impl Widget<AppData> {
-//    let textbox = TextBox::new()
-//        .lens((String, Arc<Vec<usize>>)::0)
-//        .lens(AppData::internal);
+    let textbox = TextBox::new().lens(AppData::filter);
 
-    let list = List::new(build_single_element)
-        .lens(AppData::internal);
+    let list = List::new(build_single_element);
 
     Flex::column()
-//        .with_child(textbox)
+        .with_child(textbox)
         .with_spacer(VERTICAL_WIDGET_SPACING)
         .with_child(list)
 }
 
 fn build_single_element() -> impl Widget<(String, usize)> {
-    Label::new(|(_, i): &(String, usize), _env: &Env| i.to_string())
+    Label::new(|(filter, i): &(String, usize), _env: &Env| {
+        if filter.is_empty()
+            || filter.to_lowercase().eq("all")
+            || (filter.to_lowercase().eq("odd") && i % 2 == 1)
+            || (filter.to_lowercase().eq("even") && i % 2 == 0)
+            || filter.contains(&i.to_string())
+        {
+            i.to_string()
+        } else {
+            String::from("")
+        }
+    })
 }
